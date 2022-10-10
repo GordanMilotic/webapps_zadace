91a47887f04f932384e39d9b14a26105756f1f93
Gordan Milotic <gmilotic@unipu.hr>
2022-10-10 17:00:22 +0200

    rješen zadatak 2

---
+++ zadace01/zadatak2.js
@@ -0,0 +1,10 @@
+function provjera(x){
+    if(x>=100 && x<=150000){
+        console.log(x + "je unutar intervala")
+    }
+    else {
+        console.log(x + "nije unutar intervala")
+    }
+}
+
+provjera(2345);
